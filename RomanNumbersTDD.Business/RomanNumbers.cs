6e68bdf997e69a721eb7f0cea0f65ec9b793fd9d
unknown <elifbyrkdr@gmail.com>
2020-02-23 14:14:51 +0300

    project files added

---
+++ RomanNumbersTDD.Business/RomanNumbers.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RomanNumbersTDD.Business
+{
+    public class RomanNumbers
+    {
+        private readonly Dictionary<char, int> numbers = new Dictionary<char, int>();
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public RomanNumbers()
+        {
+            numbers.Add('I', 1);
+            numbers.Add('V', 5);
+            numbers.Add('X', 10);
+            numbers.Add('L', 50);
+            numbers.Add('C', 100);
+            numbers.Add('D', 500);
+            numbers.Add('M', 1000);
+        }
+
+        /// <summary>
+        /// Converts the roman numbers to integer numbers
+        /// </summary>
+        /// <param name="romanNumber"></param>
+        /// <returns></returns>
+        public int Convert(string romanNumber)
+        {
+            if (string.IsNullOrEmpty(romanNumber) || string.IsNullOrWhiteSpace(romanNumber))
+            {
+                throw new ArgumentException();
+            }
+
+            int sum = 0;
+            for (int i = 0; i < romanNumber.Length; i++)
+            {
+                int numberZero = GetValue(romanNumber[i]);
+                int numberOne = i + 1 < romanNumber.Length ? GetValue(romanNumber[i + 1]) : -1;
+                if (numberOne == -1)
+                {
+                    sum += numberZero;
+                }
+                else if (numberZero >= numberOne)
+                {
+                    sum += numberZero;
+                }
+                else if (numberZero < numberOne)
+                {
+                    sum += numberOne - numberZero;
+                    i++;
+                }
+            }
+            return sum;
+        }
+
+        public int GetValue(char key)
+        {
+            if (numbers.TryGetValue(key, out int value))
+                return value;
+            else
+                throw new InvalidOperationException();
+        }
+    }
+}
---
+++ RomanNumbersTDD.UnitTest/RomanNumbersTests.cs
@@ -0,0 +1,51 @@
+﻿using RomanNumbersTDD.Business;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace RomanNumbersTDD.UnitTest
+{
+    public class RomanNumbersTests
+    {
+        private readonly RomanNumbers romanNumbers;
+
+        public RomanNumbersTests()
+        {
+            romanNumbers = new RomanNumbers();
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData("     ")]
+        public void Convert_WhenNumberIsInNullOrEmptyOrWhiteSpace_ThrowsException(string number)
+        {
+            Assert.Throws<ArgumentException>(() => romanNumbers.Convert(number));
+        }
+
+        [Fact]
+        public void Convert_WhenNumberIsInvalid_ThrowsException()
+        {
+            Assert.Throws<InvalidOperationException>(() => romanNumbers.Convert("MYVI"));
+        }
+
+        [Fact]
+        public void Convert_WhenNumbersIsDescendingOrder_ReturnsCorrectValue()
+        {
+            Assert.True(romanNumbers.Convert("MMVI") == 2006);
+        }
+
+        [Fact]
+        public void Convert_WhenNumbersIsDifferentOrder_ReturnsCorrectValue()
+        {
+            Assert.True(romanNumbers.Convert("MCMXLIV") == 1944);
+        }
+
+        [Fact]
+        public void Convert_WhenNumbersIsAscendingOrder_ReturnsCorrectValue()
+        {
+            Assert.True(romanNumbers.Convert("IVXL") == 44);
+        }
+    }
+}
